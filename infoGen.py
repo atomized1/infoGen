657e9d6f38ed04548041b16b1e9b055af3d903dc
atomized1 <65679821+atomized1@users.noreply.github.com>
2021-03-24 09:01:59 -0400

    Update infoGen.py

--- infoGen.py
+++ infoGen.py
@@ -2,8 +2,15 @@ import nibabel as nib
 import pandas as pd
 import os
 import sys
+import shutil
+from neuroglancer_scripts.scripts.volume_to_precomputed_pyramid import volume_to_precomputed_pyramid
+from neuroglancer_scripts.scripts.volume_to_precomputed import main as volume_to_precomputed
+from neuroglancer_scripts.scripts.generate_scales_info import main as generate_scales_info
+from neuroglancer_scripts.scripts.compute_scales import main as compute_scales
+import gzip
 
 dirnam = os.path.dirname(__file__)
+
 
 def main():
     image = nib.load(os.path.normpath(os.path.join(dirnam, sys.argv[1])))
@@ -26,7 +33,7 @@ def main():
 
     maxLabel = int(maxLabel)
 
-    info = open("info", "w")
+    info = open(os.path.normpath(os.path.join(dirnam, "info")), "w")
     info.write("{\"@type\": \"neuroglancer_segment_properties\", \"inline\": {\"ids\": [\"")
     for x in range(1, maxLabel + 1):
         info.write(str(x))
@@ -52,6 +59,48 @@ def main():
         info.write("\", \"")
     info.write(df['Alex_Abbreviation'].iat[x + 1] + "_R")
     info.write("\"]}]}}")
+
+    volume_to_precomputed(argv=["Placeholder", os.path.normpath(os.path.join(dirnam, sys.argv[1])), "--generate-info", "Output"])
+
+    fin = open(os.path.join(os.getcwd(), "Output/info_fullres.json"), "rt")
+    data = fin.read()
+    data = data.replace("float32", "uint32")
+    fin.close()
+    fout = open(os.path.join(os.getcwd(), "Output/info_fullres.json"), "wt")
+    fout.write(data)
+    fout.close()
+
+    generate_scales_info(argv=["Placeholder", "Output/info_fullres.json", "Output", "--type=segmentation"])
+    volume_to_precomputed(argv=["Placeholder", os.path.normpath(os.path.join(dirnam, sys.argv[1])), "Output"])
+    compute_scales(argv=["Placeholder", "Output", "--downscaling-method=majority"])
+
+    shutil.move(str(os.path.join(os.getcwd(), "Output")), str(os.path.join(dirnam, "Output")))
+
+    for subdir, dirs, files in os.walk(os.path.join(dirnam, "Output\\100um")):
+        #print(subdir)
+        for file in files:
+            #print(file)
+            subdirSplit = subdir.split("\\")
+            fileSplit = file.split(".")
+            fileName = subdirSplit[-2] + "_" + subdirSplit[-1] + "_" + fileSplit[0]
+            print(os.path.join(subdir, file))
+            fin = gzip.open(os.path.join(subdir, file), "r")
+            fout = open(os.path.join(dirnam, "Output/100um/", fileName), "wb")
+            data = fin.read()
+            fout.write(data)
+
+    for subdir, dirs, files in os.walk(os.path.join(dirnam, "Output\\200um")):
+        # print(subdir)
+        for file in files:
+            # print(file)
+            subdirSplit = subdir.split("\\")
+            fileSplit = file.split(".")
+            fileName = subdirSplit[-2] + "_" + subdirSplit[-1] + "_" + fileSplit[0]
+            print(os.path.join(subdir, file))
+            fin = gzip.open(os.path.join(subdir, file), "r")
+            fout = open(os.path.join(dirnam, "Output/200um/", fileName), "wb")
+            data = fin.read()
+            fout.write(data)
 
 
 if __name__ == "__main__":
